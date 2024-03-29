c1a3bfe686d25e56fa08edcc27112ae9c7c8a96f
MuthAlbert <muth.albert69@gmail.com>
2024-03-29 17:53:56 +0100

    [ORGANISATION]

---
+++ exo/exo_test/index.js
@@ -0,0 +1,13 @@
+// function changerTitre () {
+//     document.getElementById("titre").textContent = "Titre modifié";
+// }
+// changerTitre();
+
+// // Déclaration de variable en js
+// // var, let et const
+
+// CamelCase
+
+const monTitre = document.getElementById("titre");
+
+console.log(monTitre);
